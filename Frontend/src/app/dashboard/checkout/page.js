02ef21e9c9ca35591bdc4df0e2da8890d5cf4a8b
Krishnaag23 <krishnaag23@iitk.ac.in>
2025-01-03 14:50:52 +0530

    Streamlined Preorder #03

--- Frontend/src/app/dashboard/checkout/page.js
+++ Frontend/src/app/dashboard/checkout/page.js
@@ -1,34 +1,31 @@
-'use client'
+"use client";
 
-import CheckoutForm from '@/components/Checkout/CheckoutForm'
-import OrderSummary from '@/components/Checkout/OrderSummary'
-import Breadcrumb from '@/components/Common/Breadcrumb'
-
+import CheckoutForm from "@/components/Checkout/CheckoutForm";
+import OrderSummary from "@/components/Checkout/OrderSummary";
+import Breadcrumb from "@/components/Common/Breadcrumb";
 
 export default function Checkout() {
-  
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-transparent">
-      
       <Breadcrumb
         pageName="Checkout"
         description="Complete your purchase by filling in the form and reviewing your order details."
       />
 
       {/* Main content */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-32">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+      {/* <div className="mx-auto max-w-7xl px-4 pb-32 sm:px-6 lg:px-8"> */}
+        {/* <div className="grid grid-cols-1 gap-8 lg:grid-cols-2"> */}
           {/* Checkout Form */}
-          <div className="bg-white p-6 rounded-lg shadow-md dark:bg-gray-800">
+          <div className="rounded-lg bg-white p-6 shadow-md dark:bg-gray-800">
             <CheckoutForm />
           </div>
 
           {/* Order Summary */}
-          <div className="bg-white p-6 rounded-lg shadow-md dark:bg-gray-800">
+          {/* <div className="bg-white p-6 rounded-lg shadow-md dark:bg-gray-800">
             <OrderSummary />
-          </div>
-        </div>
-      </div>
+          </div> */}
+        {/* </div> */}
+      {/* </div> */}
     </div>
-  )
+  );
 }
